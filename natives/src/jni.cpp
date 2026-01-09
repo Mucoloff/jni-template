c75f3356382a674c25781b4f1c3e4334ff454d7b
Mucoloff <lupica.francy06@gmail.com>
2026-01-09 01:31:43 +0100

    Refactor JNI method signatures and update implementation for sum and subtract

--- natives/src/jni.cpp
+++ natives/src/jni.cpp
@@ -3,13 +3,11 @@
 #include "dev_sweety_NativeApi.h"
 
 extern "C" {
-Engine engine;
-
-JNIEXPORT jint JNICALL Java_dev_sweety_NativeApi_sum(JNIEnv* env, jobject thiz, jint a, jint b) {
-    return static_cast<jint>(engine.sum(static_cast<int>(a), static_cast<int>(b)));
-}
+JNIEXPORT jint JNICALL Java_dev_sweety_NativeApi_sum(JNIEnv* env, jobject thiz, const jint a, const jint b) {
+    return Engine::sum(a, b);
 }
 
-JNIEXPORT jint JNICALL Java_dev_sweety_NativeApi_subtract(JNIEnv*, jobject thiz, jint a, jint b) {
-    return static_cast<jint>(engine.subtract(static_cast<int>(a), static_cast<int>(b)));
+JNIEXPORT jint JNICALL Java_dev_sweety_NativeApi_subtract(JNIEnv*, jobject thiz, const jint a, const jint b) {
+    return Engine::subtract(a, b);
+}
 }
